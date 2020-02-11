039b4c64dd6e9421707dfc355f359ced55647fed
aa-010 <290949@gmail.com>
2020-02-11 12:06:18 +0200

    Create Phonebook.py

---
+++ Phonebook.py
@@ -0,0 +1,26 @@
+from Contact import Contact
+
+class PhoneBook:
+    __contact = []    
+    
+    def add(self, cont):
+        self.__contact.append(cont)
+
+
+
+
+
+
+
+
+#import msvcrt
+#import sys
+ 
+#while True:
+#    pressedKey = msvcrt.getch()
+#    if ord(pressedKey) == ord('q'):    
+#       sys.exit()
+#    else:
+#       print ("Key Pressed:", ord(pressedKey))
+
+
